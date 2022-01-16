07e446017351c63360c3a4ffc13e944587ba0f13
SHURAdavi <shuradavi@gmail.com>
2022-01-16 22:26:24 +0300

    updated

---
+++ index.js
@@ -0,0 +1,11 @@
+let $game = document.querySelector('#game')
+let $start = document.querySelector('#start')
+
+
+$start.addEventListener('click', function () {
+$start.classList.add('hide')
+$game.style.backgroundColor = '#fff'
+})
+
+$game.setAttribute('box')
+
